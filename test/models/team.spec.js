95c82a37647dd4f72d05e096f43a94cd4f75b9a8
plapicola <p.lapicola@gmail.com>
2019-06-02 17:10:51 -0600

    Adds testing for team model

---
+++ test/models/team.spec.js
@@ -0,0 +1,37 @@
+var Team = require('../../models/Team');
+var shell = require('shelljs');
+var createdId;
+
+describe("Team model", () => {
+  beforeAll(() => {
+    shell.exec('npx knex migrate:rollback --all');
+    shell.exec('npx knex migrate:latest');
+  })
+
+  test("Create", () => {
+    return Team.create({name: "Canada"})
+      .then(([team]) => {
+        expect(typeof(team.id)).toBe("number")
+        createdId = team.id
+        expect(team.name).toBe("Canada")
+      })
+  })
+
+  test("All", () => {
+    return Team.all()
+      .then(teams => {
+        expect(teams.length).toBe(1)
+        [team] = teams;
+        expect(team.id).toBe(createdId);
+        expect(team.name).toBe("Canada")
+      })
+  })
+
+  test("Find", () => {
+    return Team.find(createdId)
+      .then(team => {
+        expect(team.id).toBe(createdId)
+        expect(team.name).toBe("Canada")
+      })
+  })
+})
