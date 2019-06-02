9beef168815d044993e521aede100a21aceed26f
plapicola <p.lapicola@gmail.com>
2019-06-02 17:29:28 -0600

    Updates olympian spec to correct for requirements on team_id

--- test/models/olympian.spec.js
+++ test/models/olympian.spec.js
@@ -1,15 +1,19 @@
 var Olympian = require('../../models/olympian')
+var Team = require('../../models/team')
 var shell = require('shelljs')
 var firstId;
+var teamId;
 
 describe('Olympian Model', () => {
-  beforeAll(() => {
+  beforeAll(async () => {
     shell.exec('npx knex migrate:rollback --all');
     shell.exec('npx knex migrate:latest');
+    await Team.create({name: "Canada"})
+    .then(([team]) => teamId = team.id)
   })
 
   test("Create", () => {
-    return Olympian.create({name: "Test!", age: 25, height: 150, weight: 140, sex: "M"})
+    return Olympian.create({name: "Test!", team_id: teamId, age: 25, height: 150, weight: 140, sex: "M"})
       .then(([olympian]) => {
         expect(typeof(olympian.id)).toBe("number")
         firstId = olympian.id;
@@ -18,6 +22,7 @@ describe('Olympian Model', () => {
         expect(olympian.height).toBe(150)
         expect(olympian.weight).toBe(140)
         expect(olympian.sex).toBe("M")
+        expect(olympian.team_id).toBe(teamId)
       })
   })
 
@@ -32,6 +37,7 @@ describe('Olympian Model', () => {
         expect(olympian.height).toBe(150)
         expect(olympian.weight).toBe(140)
         expect(olympian.sex).toBe("M")
+        expect(olympian.team_id).toBe(teamId)
       })
   })
 
@@ -44,6 +50,7 @@ describe('Olympian Model', () => {
         expect(olympian.height).toBe(150)
         expect(olympian.weight).toBe(140)
         expect(olympian.sex).toBe("M")
+        expect(olympian.team_id).toBe(teamId)
       })
   })
 })
