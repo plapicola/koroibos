314f04a53ba0fb84836d1b656fbf26c32e09d4bf
plapicola <p.lapicola@gmail.com>
2019-06-02 15:43:02 -0600

    Adds olympian find test

--- test/models/olympian.spec.js
+++ test/models/olympian.spec.js
@@ -7,10 +7,13 @@ describe('Olympian Model', () => {
     shell.exec('npx knex migrate:latest');
   })
 
+  const firstId;
+
   test("Create", () => {
     return Olympian.create({name: "Test!", age: 25, height: 150, weight: 140, sex: "M"})
       .then(([olympian]) => {
         expect(typeof(olympian.id)).toBe("number")
+        firstId = olympian.id;
         expect(olympian.name).toBe("Test!")
         expect(olympian.age).toBe(25)
         expect(olympian.height).toBe(150)
@@ -24,6 +27,18 @@ describe('Olympian Model', () => {
       .then(olympians => {
         expect(olympians.length).toBe(1)
         let olympian = olympians[0]
+        expect(typeof(olympian.id)).toBe("number")
+        expect(olympian.name).toBe("Test!")
+        expect(olympian.age).toBe(25)
+        expect(olympian.height).toBe(150)
+        expect(olympian.weight).toBe(140)
+        expect(olympian.sex).toBe("M")
+      })
+  })
+
+  test("find", () => {
+    return Olympian.one(firstId)
+      .then(olympian => {
         expect(typeof(olympian.id)).toBe("number")
         expect(olympian.name).toBe("Test!")
         expect(olympian.age).toBe(25)
