718fd07082ebb2b98d324bb4f413c154904e35b7
plapicola <p.lapicola@gmail.com>
2019-06-02 19:44:35 -0600

    Updates expectation in olympian_event test to correct for typo

--- test/models/olympian_event.spec.js
+++ test/models/olympian_event.spec.js
@@ -12,7 +12,7 @@ describe('OlympianEvent model', () => {
     shell.exec('npx knex migrate:latest')
     await Team.create({name: "Canada"}).then(([team]) => teamId = team.id)
     await Sport.create({name: "Athletics"}).then(([sport]) => sportId = sport.id)
-    await Olympian.create(Olympian.create({name: "Test!", team_id: teamId, sport_id: sportId, age: 25, height: 150, weight: 140, sex: "M"}))
+    await Olympian.create({name: "Test!", team_id: teamId, sport_id: sportId, age: 25, height: 150, weight: 140, sex: "M"})
       .then(([olympian]) => olympianId = olympian.id)
     await Event.create({name: "400M Sprint", sport_id: sportId}).then(([e]) => eventId = e.id)
   })
@@ -34,7 +34,7 @@ describe('OlympianEvent model', () => {
         olympianEvent = olympian_events[0]
         expect(olympianEvent.id).toBe(olympianEventId)
         expect(olympianEvent.olympian_id).toBe(olympianId)
-        expect(olympianEvent.eventId).toBe(eventId)
+        expect(olympianEvent.event_id).toBe(eventId)
       })
   })
 
