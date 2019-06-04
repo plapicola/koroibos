ee19f53bd5ef72756945f9c8f1bee45defd7d899
plapicola <p.lapicola@gmail.com>
2019-06-03 18:04:48 -0600

    Adds olympian stats facade

---
+++ facades/olympian_stats.js
@@ -0,0 +1,16 @@
+var Olympian = require('../models/olympian')
+
+module.exports = class OlympianStatsIndexFacade {
+  constructor(status, body) {
+    this.status = status
+    this.body = body
+  }
+
+  static get_statistics() {
+    return new Promise((resolve, reject) => {
+      Olympian.statistics()
+      .then(statistics => resolve(new OlympianStatsIndexFacade(200, statistics)))
+      .catch(error => reject(new OlympianStatsIndexFacade(500, error)))
+    })
+  }
+}
